bae28e1c1f378c2afcc1ed16655ac56c85690d78
blackpinkbp <phihungadgjmptw9a9@gmail.com>
2020-11-14 12:12:42 +0700

    2nd: import static modification

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -5,8 +5,9 @@
  */
 package math.util;
 
-import hungnp.util.MathUtility;
-
+import static hungnp.util.MathUtility.*;
+//lệnh mới từ JDK 8-5, khai báo sẵn hàm static
+//Ở dưới gọi hàm static ko cần chấm, giống C. chỉ ap dụng cho static
 /**
  *
  * @author Win 10
@@ -20,16 +21,19 @@ public class MathUtil {
         // ta test thử hàm của ta
         // dùng mắt để so sánh kết quả thực tế khi chạy và kết quả mong đợi
         long excepted= 120;//ta mong nhận 120 nếu gọi hàm 5!
-        long actual=MathUtility.getFactorial(5);
+        long actual=getFactorial(5);
         System.out.println("Excepted: "+excepted+"; Actual: "+actual);
         //excepted 720 if tinh 6!
-        System.out.println("6!: "+MathUtility.getFactorial(6));
+        System.out.println("6!: "+getFactorial(6));
         
         //excepted 1 if tinh 0
-        System.out.println("0!: "+MathUtility.getFactorial(0));
+        System.out.println("0!: "+getFactorial(0));
         
         //excepted IllegalArgumentException if tinh -5!
-        System.out.println("-5!: "+MathUtility.getFactorial(-5));
+        System.out.println("-5!: "+getFactorial(-5));
+        
+        //in thử PI coi có giống Math.PI
+        System.out.println("PI: "+PI);
     }
     
 }
